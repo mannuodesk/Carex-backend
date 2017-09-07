e6509c4609629d79497f2dd2ec77d9516c105989
Ali Sajjad <rizvisyed12@gmail.com>
2017-09-07 21:29:55 +0500

    ABC

--- routes/purchase.js
+++ routes/purchase.js
@@ -56,97 +56,112 @@ global.rateForToken = 0.52;
 postPurchaseTokens.post(function (req, res) {
     var user = new User();
     var response = new Response();
-    user.Email = req.body.Email;
-    user.WalletAddress = req.body.WalletAddress;
-    user.WalletType = req.body.WalletType;
-    user.Tokens = req.body.Tokens;
-    user.UpdatedOnUTC = Math.floor(new Date());
-    user.CreatedOnUTC = Math.floor(new Date());
-    user.save();
-    if (user.WalletType == WalletTypeEnum.BITCOIN) {
-        var currency = '';
-        if (req.body.WalletType == CryptoTypeEnum.BITCOIN) {
-            currency = 'BTC';
+    User.findOne({WalletAddress:req.body.WalletAddress}, function(err, user){
+        if(err){
+            console.log(err);
         }
-        else if (req.body.WalletType == CryptoTypeEnum.ETHEREUM) {
-            currency = 'ETH';
-        }
-        if (currency != 'ETH') {
-            var confirmationUrl = "https://chain.so/api/v2/is_tx_confirmed/" + currency + "/" + req.body.TxHash;
-            var transactionDataUrl = "https://chain.so/api/v2/get_tx_outputs/" + currency + "/" + req.body.TxHash;
-            client.get(confirmationUrl, function (data, r) {
-                // parsed response body as js object 
-                if (data.data.is_confirmed == true) {
-
-                    //Work to be done
-                    //Transaction Address to be verified
-                    //Transaction Amount to be verified and calculation to be done
-                    //multiplication of final calculation with decimals
-
-
-                    client.get(transactionDataUrl, function (data, r) {
-
-                        var outputsArray = data.data.outputs;
-                        var confirmationCount = 0;
-                        var totalBTCValue = 0;
-                        for (var i = 0; i < outputsArray.length; i++) {
-                            if (outputsArray[i].address == req.body.WalletAddress || outputsArray[i].address == global.AdminBitcoinAddress){
-                                confirmationCount = confirmationCount + 1;
-                            }
-                            if(outputsArray[i].address == req.body.WalletAddress){
-                                totalBTCValue = parseFloat(outputsArray[i].value);
-                            }
-                        }
-                        if (confirmationCount >= 2) {
-                            var now = Math.floor(new Date());
-                            var bonus = 0;
-                            if (now < global.preSaleBonusDays) {
-                                bonus = preSaleBonusPercent;
-                            }
-                            else if (now < global.saleBonusDays) {
-                                bonus = saleBonusPercent;
-                            }
-
-                            var tokensToTransfer = (totalBTCValue / global.priceForToken);
-                            var bonusTokens = (tokensToTransfer * bonus) * 100;
-                            tokensToTransfer = tokensToTransfer + bonusTokens;
-                            tokenUtil.transferToken("0xc8ae3f27017b99fd4072983c0a254174500441bf", "inw76IzO27d9a03aceef5f2d22f65ab3c0560a19f", req.body.EthAddress, tokensToTransfer);
-                            response.code = ResponseCodeEnum.SUCCESS;
-                            response.data = null;
-                            response.message = "Success";
-                            res.json(response);
+        if(user == null){
+            user.Email = req.body.Email;
+            user.WalletAddress = req.body.WalletAddress;
+            user.WalletType = req.body.WalletType;
+            user.Tokens = req.body.Tokens;
+            user.UpdatedOnUTC = Math.floor(new Date());
+            user.CreatedOnUTC = Math.floor(new Date());
+            
+            if (user.WalletType == WalletTypeEnum.BITCOIN) {
+                var currency = '';
+                if (req.body.WalletType == CryptoTypeEnum.BITCOIN) {
+                    currency = 'BTC';
+                }
+                else if (req.body.WalletType == CryptoTypeEnum.ETHEREUM) {
+                    currency = 'ETH';
+                }
+                if (currency != 'ETH') {
+                    var confirmationUrl = "https://chain.so/api/v2/is_tx_confirmed/" + currency + "/" + req.body.TxHash;
+                    var transactionDataUrl = "https://chain.so/api/v2/get_tx_outputs/" + currency + "/" + req.body.TxHash;
+                    client.get(confirmationUrl, function (data, r) {
+                        // parsed response body as js object 
+                        if (data.data.is_confirmed == true) {
+        
+                            //Work to be done
+                            //Transaction Address to be verified
+                            //Transaction Amount to be verified and calculation to be done
+                            //multiplication of final calculation with decimals
+        
+        
+                            client.get(transactionDataUrl, function (data, r) {
+        
+                                var outputsArray = data.data.outputs;
+                                var confirmationCount = 0;
+                                var totalBTCValue = 0;
+                                for (var i = 0; i < outputsArray.length; i++) {
+                                    if (outputsArray[i].address == req.body.WalletAddress || outputsArray[i].address == global.AdminBitcoinAddress){
+                                        confirmationCount = confirmationCount + 1;
+                                    }
+                                    if(outputsArray[i].address == req.body.WalletAddress){
+                                        totalBTCValue = parseFloat(outputsArray[i].value);
+                                    }
+                                }
+                                if (confirmationCount >= 2) {
+                                    var now = Math.floor(new Date());
+                                    var bonus = 0;
+                                    if (now < global.preSaleBonusDays) {
+                                        bonus = preSaleBonusPercent;
+                                    }
+                                    else if (now < global.saleBonusDays) {
+                                        bonus = saleBonusPercent;
+                                    }
+        
+                                    var tokensToTransfer = (totalBTCValue / global.priceForToken);
+                                    var bonusTokens = (tokensToTransfer * bonus) * 100;
+                                    tokensToTransfer = tokensToTransfer + bonusTokens;
+                                    tokenUtil.transferToken("0xc8ae3f27017b99fd4072983c0a254174500441bf", "inw76IzO27d9a03aceef5f2d22f65ab3c0560a19f", req.body.EthAddress, tokensToTransfer);
+                                    user.save();
+                                    response.code = ResponseCodeEnum.SUCCESS;
+                                    response.data = null;
+                                    response.message = "Success";
+                                    res.json(response);
+                                }
+                                else {
+                                    response.code = ResponseCodeEnum.ERRORED;
+                                    response.message = "Transaction Not Confirmed by System";
+                                    response.data = null;
+                                    res.json(response);
+                                }
+                            });
                         }
                         else {
                             response.code = ResponseCodeEnum.ERRORED;
-                            response.message = "Transaction Not Confirmed by System";
+                            response.message = "Transaction Not Confirmed";
                             response.data = null;
-                            res.json(response);
+                            res.json(res);
                         }
                     });
-
-
                 }
                 else {
-                    response.code = ResponseCodeEnum.ERRORED;
-                    response.message = "Transaction Not Confirmed";
+                    user.save();
+                    response.code = ResponseCodeEnum.SUCCESS;
                     response.data = null;
-                    res.json(res);
+                    response.message = "Success";
+                    res.json(response);
                 }
-            });
+            }
+            else {
+                user.save();
+                response.code = ResponseCodeEnum.SUCCESS;
+                response.data = null;
+                response.message = "Success";
+                res.json(response);
+            }
         }
-        else {
+        else{
             response.code = ResponseCodeEnum.SUCCESS;
             response.data = null;
             response.message = "Success";
             res.json(response);
         }
-    }
-    else {
-        response.code = ResponseCodeEnum.SUCCESS;
-        response.data = null;
-        response.message = "Success";
-        res.json(response);
-    }
+    });
+    
 });
 
 getPurchasersList.get(function (req, res) {
@@ -163,7 +178,8 @@ getPurchasersList.get(function (req, res) {
                 }
                 else {
                     try{
-                        purchasers[i].Ethers = tokenUtil.etherOf(purchasers[i].WalletAddress);
+                        var eth = parseFloat(tokenUtil.etherOf(purchasers[i].WalletAddress));
+                        purchasers[i].Ethers = eth/1000;
                         purchasers[i].Password = "ETH";
                     }
                     catch(ex){
